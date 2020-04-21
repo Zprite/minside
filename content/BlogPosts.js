5747da9927db12836a19930a3ae121a035656d80
unknown <birkelandmarcus@gmail.com>
2020-04-22 00:17:16 +0200

    updated a description

--- content/BlogPosts.js
+++ content/BlogPosts.js
@@ -1,6 +1,6 @@
 export const posts = [
     {date: "2020-02-22", title:"terminus" , desc:"Jobbkalender og lønnsoversikt for de som jobber skift eller deltid!", img:"https://i.imgur.com/vFXqxh9.jpg"}
-    ,{date: "2015-07-30", title:"Prosjekt N", desc:"En mod til Super Smash Bros Melee", img:"https://scontent.fosl3-2.fna.fbcdn.net/v/t31.0-8/12052554_1644910162441416_196696005697174009_o.jpg?_nc_cat=111&_nc_sid=07e735&_nc_ohc=kNiDFtYn_DgAX875Lgl&_nc_ht=scontent.fosl3-2.fna&oh=3b52c488b6377ce910ccafed11ef1984&oe=5EC0FC9B"}
+    ,{date: "2015-07-30", title:"Prosjekt N", desc:"En mod til Super Smash Bros Melee hvor de fleste karakterene får et norskt særpreg!", img:"https://scontent.fosl3-2.fna.fbcdn.net/v/t31.0-8/12052554_1644910162441416_196696005697174009_o.jpg?_nc_cat=111&_nc_sid=07e735&_nc_ohc=kNiDFtYn_DgAX875Lgl&_nc_ht=scontent.fosl3-2.fna&oh=3b52c488b6377ce910ccafed11ef1984&oe=5EC0FC9B"}
     ,{date: "2019-01-15", title:"Matrise-klokke", desc:"En klokke med grunnleggende funksjoner drevet av et 16x16 LED panel, Arduino og RTC. Opprinnelig et skoleprosjekt rundt mikrokontrollere.",
         img:"https://i.imgur.com/5a3AIRU.png"},
     ,{date: "2019-03-04", title:"Bluetooth-display", desc:"Et 40x16 display drevet av Arduino og MAX7219. Displayet blir styrt av en Android app via Bluetooth.", img:"https://i.imgur.com/RWr5yts.jpg"}
